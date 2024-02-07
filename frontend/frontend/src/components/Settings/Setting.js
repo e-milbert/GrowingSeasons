975ea905252f8d945f1063c4ecb8c7cbbc4e66d7
elena milbert <elena.milbert@muehlbauer.de>
2024-02-07 09:58:56 +0100

    settings view added

---
+++ frontend/frontend/src/components/Settings/Setting.js
@@ -0,0 +1,112 @@
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
+import {OneLineFormC} from "../formHelpers/OneLineFormC";
+import {useState} from "react";
+import {updatePostWeatherSettings} from "../../constants/apiConstants";
+import {LoadingAnimation} from "../shared/LoadingAnimation";
+
+export function Setting() {
+
+    const defaultPref = {
+        id: 1,
+        longitude: 52.51,
+        latitude: 13.37,
+
+    }
+
+
+    const [weatherPreference, setWeatherPreference] = useState(defaultPref);
+    const [loadingWeatherUpdate, setLoadingWeatherUpdate] = useState(false);
+
+    const updateWeather = async () => {
+
+
+        if (weatherPreference.latitude <= 90 && weatherPreference.latitude >= -90 && weatherPreference.longitude <= 180 && weatherPreference.longitude >= -180) {
+            setLoadingWeatherUpdate(true);
+
+            try {
+                const settings = JSON.stringify(weatherPreference);
+
+                const response = await fetch(updatePostWeatherSettings, {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: settings
+                });
+
+                if (response.ok) {
+                    console.log("preferences updated")
+                } else if (response.status === 503) {
+                    console.log("service is not available, weather data cannot be retrieved from third party")
+                } else if (response.status === 400) {
+                    console.log("something is wrong with the request url sent to the weather data provider. check logs")
+                }
+            } catch (error) {
+                console.log(error);
+            }
+
+            setLoadingWeatherUpdate(false);
+
+        }
+    }
+    const handleLatitude = (value) => {
+
+        setWeatherPreference({...weatherPreference, latitude: value})
+
+
+    }
+    const handleLongitude = (value) => {
+        setWeatherPreference({...weatherPreference, longitude: value})
+    }
+
+
+//TODO settings for weather api: longitude, latitude, timezone(should be dropdown)
+
+    return (
+        <>
+
+            <Row>
+                <Col>
+                    <Container className={"bg-container d-flex justify-content-center p-5 rounded-3"}>
+                        <Card className={"bg-transparent m-2 p-2 border-0"}>
+                            <Card.Title className={"text-white"}>weather settings</Card.Title>
+                            <Card.Body>
+                                <Row className={"d-flex justify-content-evenly"}>
+                                    <Col>
+                                        <OneLineFormC handleInputChangeFunction={handleLatitude}
+                                                      idName={"lat"}
+                                                      labelText={"Latitude"} placeholderText={""}/>
+
+                                    </Col>
+                                    <Col>
+                                        <OneLineFormC handleInputChangeFunction={handleLongitude}
+                                                      idName={"long"}
+                                                      labelText={"Longitude"} placeholderText={""}/>
+                                    </Col>
+                                    <Col>
+                                        {!loadingWeatherUpdate ?
+                                        <Button
+                                                className={"custom-button text-black p-2"} type={"button"}
+                                                onClick={updateWeather}>submit</Button> :
+                                                <i className='bx bxs-florist text-white bx-tada bx-md'/>
+                                            }
+                                    </Col>
+                                </Row>
+                                {weatherPreference.latitude > 90 || weatherPreference.latitude < -90 || weatherPreference.longitude > 180 || weatherPreference.longitude < -180 ?
+                                    <Row>
+                                        <Col>
+                                            <div className={"ms-5 fs-6 fw-normal text-warning-emphasis"}>latitude is from -180 to 180and longitude is from -90 to 90 </div>
+                                        </Col>
+                                    </Row>:null
+                                }
+                            </Card.Body>
+
+                        </Card>
+                    </Container>
+                </Col>
+            </Row>
+        </>
+    )
+
+}
+
--- frontend/frontend/src/components/shared/NavigationC.js
+++ frontend/frontend/src/components/shared/NavigationC.js
@@ -7,6 +7,7 @@ import {DatabaseParent} from "../databaseview/DatabaseC";
 import {TimeLineTable} from "../timeline/TimeLineTable";
 import {NewDbEntryC} from "../newentry/NewDbEntryC";
 import {shutdownInit} from "../../constants/apiConstants";
+import {Setting} from "../Settings/Setting";
 
 
 export function NavigationC() {
@@ -37,7 +38,7 @@ export function NavigationC() {
                     <Navbar id="navbar-main" expand="lg" className="custom-header">
                         <Container className="mt-5">
                             <Navbar.Brand href="/" className="nav-font-style"><i
-                                className='bx bx-md bxs-leaf align-middle'></i></Navbar.Brand>
+                                className='bx bx-lg bxs-leaf align-middle bx-tada-hover'></i></Navbar.Brand>
                             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                             <Navbar.Collapse id="basic-navbar-nav">
                                 <Nav className="me-auto">
@@ -45,9 +46,11 @@ export function NavigationC() {
                                     <Nav.Link className="nav-font-style" href="/entries">Database</Nav.Link>
                                     <Nav.Link className="nav-font-style" href="/addtodatabase">
                                         Entry Form</Nav.Link>
+                                    <Nav.Link className="nav-font-style" href="/settings"><i className='bx bxs-cog bx-sm bx-spin-hover align-middle '/></Nav.Link>
                                 </Nav>
                                 <Button className="bg-transparent border-0 text-white text-end"
-                                        onClick={() => setWasShutdown(true)}>shut down</Button>
+                                        onClick={() => setWasShutdown(true)}><i
+                                    className='bx bx-power-off bx-md'/></Button>
                             </Navbar.Collapse>
                         </Container>
                     </Navbar>
@@ -59,6 +62,7 @@ export function NavigationC() {
                             <Route path="/timeline" element={<TimeLineTable/>}/>
                             <Route path="/entries" element={<DatabaseParent/>}/>
                             <Route path="/addtodatabase" element={<NewDbEntryC/>}/>
+                            <Route path="/settings" element={<Setting/>}/>
                         </Routes>
                     </Container>
 
